a952ce82c213ef349ef04818e63b906922ff3631
Bryan Orozco <bryanorozcoweb@gmail.com>
2017-12-15 11:12:09 -0800

    Updated socket app

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-var http = require('http');
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+http.listen(process.env.PORT || 3000);
 
-var server = http.createServer(app);
-// Pass a http.Server instance to the listen method
-var io = require('socket.io').listen(server);
-
-server.listen(3000);
-
-app.use(express.static(__dirname + '/node_modules'));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 
