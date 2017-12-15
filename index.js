b3bd5d95b264ec43d51eeac5d086bc740dd25aca
Bryan Orozco <bryanorozcoweb@gmail.com>
2017-12-15 09:59:12 -0800

    Testing Heroku server configurations

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
 
-});
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
 
-app.get('/admin', function(req, res){
-  res.sendFile(__dirname + '/admin.html');
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-});
+  const io = socketIO(server);
+
+// var express = require('express');
+// var app = express();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+//
+// });
+
+// app.get('/admin', function(req, res){
+//   res.sendFile(__dirname + '/admin.html');
+//
+// });
 
 var color = "#000";
 var campus = '';
@@ -37,7 +51,7 @@ io.on('connection', function (socket) {
 
 });
 
-app.use('/images', express.static(__dirname + "/images"));
+// app.use('/images', express.static(__dirname + "/images"));
 
 
 // http.listen(8080, function(){
